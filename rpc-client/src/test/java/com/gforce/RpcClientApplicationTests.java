46795be6c17b8a73d0150dbb5f1a1f70b4046f3d
gforce <448031739@qq.com>
2020-10-05 21:04:01 +0800

    修改pom文件

--- rpc-client/src/test/java/com/gforce/RpcClientApplicationTests.java
+++ rpc-client/src/test/java/com/gforce/RpcClientApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class RpcClientApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
--- rpc-core/src/main/java/com/gforce/RpcCoreApplication.java
+++ rpc-core/src/main/java/com/gforce/RpcCoreApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RpcCoreApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RpcCoreApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RpcCoreApplication.class, args);
+    }
 
 }
